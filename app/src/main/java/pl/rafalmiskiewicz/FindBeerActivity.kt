505639e75de80b3e903f443f8efa3e04cc029ef6
rmiskiewicz <rmiskiewicz@corelogic.pl>
2021-02-11 16:57:54 +0100

    next Klasa BeerExpert

---
+++ app/src/main/java/pl/rafalmiskiewicz/FindBeerActivity.kt
@@ -0,0 +1,21 @@
+package pl.rafalmiskiewicz
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
+
+class FindBeerActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_beer)
+    }
+
+    fun onClickFindBeer(view: View) {
+        var brands: TextView = findViewById(R.id.brands)
+        var color: Spinner = findViewById(R.id.color)
+        var beerType: String = color.selectedItem.toString()
+        brands.text = beerType
+    }
+}
