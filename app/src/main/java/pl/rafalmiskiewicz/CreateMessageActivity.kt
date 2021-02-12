4d069470dddc7d404cd19082eb9da3a07fd36628
Rafał Miśkiewicz <rmiskiewicz@corelogic.pl>
2021-02-12 16:54:52 +0100

    pierwsza aktywność

--- app/src/main/java/pl/rafalmiskiewicz/CreateMessageActivity.kt
+++ app/src/main/java/pl/rafalmiskiewicz/CreateMessageActivity.kt
@@ -2,10 +2,15 @@ package pl.rafalmiskiewicz
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 
 class CreateMessageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_message)
+    }
+
+    fun onSendMessage(view: View){
+
     }
 }
