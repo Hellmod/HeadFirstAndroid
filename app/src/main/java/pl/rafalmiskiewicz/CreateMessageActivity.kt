76a5a56208062ab6abb23af11c37fc011a388370
rmiskiewicz <rmiskiewicz@corelogic.pl>
2021-02-15 15:22:41 +0100

    intent.putExtra przekazywanie zmiennych między aktywnościami

--- app/src/main/java/pl/rafalmiskiewicz/CreateMessageActivity.kt
+++ app/src/main/java/pl/rafalmiskiewicz/CreateMessageActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class CreateMessageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,11 @@ class CreateMessageActivity : Activity() {
         setContentView(R.layout.activity_create_message)
     }
 
-    fun onSendMessage(view: View){
-        var intentt = Intent(this,ReceiveMessageActivity::class.java)
-                startActivity(intentt)
+    fun onSendMessage(view: View) {
+        val intent = Intent(this, ReceiveMessageActivity::class.java)
+        val textView: TextView = findViewById(R.id.message)
+        val messageText: String = textView.text.toString()
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText)
+        startActivity(intent)
     }
 }
--- app/src/main/java/pl/rafalmiskiewicz/ReceiveMessageActivity.kt
+++ app/src/main/java/pl/rafalmiskiewicz/ReceiveMessageActivity.kt
@@ -1,11 +1,22 @@
 package pl.rafalmiskiewicz
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class ReceiveMessageActivity : AppCompatActivity() {
+class ReceiveMessageActivity : Activity() {
+
+    companion object {
+        const val EXTRA_MESSAGE: String = "message"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receive_message)
+
+        val messageText = intent.getStringExtra(EXTRA_MESSAGE)
+        val messageView: TextView = findViewById(R.id.message)
+        messageView.text=messageText
     }
 }
