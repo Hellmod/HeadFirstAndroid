f010678b5611d28688fda3808c1eabcd7f38fbd5
Rafał Miśkiewicz <rafalmiskiewicz9@gmail.com>
2021-03-09 23:20:27 +0100

    metoda runTimer()

--- app/src/main/java/pl/rafalmiskiewicz/StopwatchActivity.kt
+++ app/src/main/java/pl/rafalmiskiewicz/StopwatchActivity.kt
@@ -2,10 +2,33 @@ package pl.rafalmiskiewicz
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.TextureView
+import android.view.View
 
 class StopwatchActivity : Activity() {
+    var seconds = 0
+    var running = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stopwatch)
+    }
+
+    public fun onClickStart(view: View) {
+        running = true
+    }
+
+    public fun onClickStop(view: View) {
+        running = false
+    }
+
+    public fun onClickReset(view: View) {
+        running = false
+        seconds = 0
+    }
+
+    fun runTimer() {
+        val textView = findViewById<TextureView>(R.id.time_view)
+
     }
 }
