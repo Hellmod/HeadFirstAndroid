998a8b3e1df15c9b10337bb36125a19493976f7c
Rafał Miśkiewicz <rafalmiskiewicz9@gmail.com>
2021-03-26 13:20:46 +0100

    init

--- app/src/main/java/pl/rafalmiskiewicz/TopLevelActivity.kt
+++ app/src/main/java/pl/rafalmiskiewicz/TopLevelActivity.kt
@@ -3,9 +3,9 @@ package pl.rafalmiskiewicz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TopLevelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_top_level)
     }
 }
